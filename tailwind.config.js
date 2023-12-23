887ed908f1151fc78044e291a75c6f0efa64e284
Moni-Yahwehson <moniyahwehson@gmail.com>
2023-12-23 21:11:50 +0100

    Working with custom fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,16 @@
 /* eslint-disable import/no-anonymous-default-export */
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    fontFamily: {
+      sans: 'Roboto Mono, Gill Sans MT Regular',
+    },
+    extend: {
+      height: {
+        screen: '100dvh',
+      },
+    },
   },
   plugins: [],
 };
