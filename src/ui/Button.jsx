a9ac776c5418793070ad918647b5c2f5ad67b269
Moni-Yahwehson <moniyahwehson@gmail.com>
2023-12-23 18:03:17 +0100

    More styling

--- src/ui/Button.jsx
+++ src/ui/Button.jsx
@@ -1,5 +1,20 @@
-function Button() {
-  return <div></div>;
+import { Link } from 'react-router-dom';
+
+/* eslint-disable react/prop-types */
+function Button({ children, disabled, to }) {
+  const className =
+    'inline-block rounded-fullbg-yellow-400 px-4 py-3 font-semibolduppercase tracking-wide text-stone-800 transition-colorsduration-300 hover:bg-yellow-300 focus:bg-yellow-300 focus:outline-nonefocus:ring focus:ring-yellow-300 focus:ring-offset-2 disabled:cursor-not-allowed sm:px-6 sm:py-4';
+  if (to)
+    return (
+      <Link to={to} className={className}>
+        {children}
+      </Link>
+    );
+  return (
+    <button disabled={disabled} className={className}>
+      {children}
+    </button>
+  );
 }
 
 export default Button;
