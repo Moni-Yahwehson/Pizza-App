0d9b7e0d23f43518e9e8f5ab2def96796f3d94ac
Moni-Yahwehson <moniyahwehson@gmail.com>
2023-12-25 14:26:04 +0100

    Working on the cart state

---
+++ src/features/cart/cartSlice.js
@@ -0,0 +1,31 @@
+import { createSlice } from '@reduxjs/toolkit';
+const initialState = {
+  //   cart: [],
+  cart: {
+    pizzaId: 12,
+    name: 'Mediterranean',
+    quantity: 2,
+    unitPrice: 16,
+    totalPrice: 32,
+  },
+};
+
+const cartSlice = createSlice({
+  name: 'cart',
+  initialState,
+  reducers: {
+    addItem(state, action) {
+      //payload = newItem
+      state.cart.push(action.payload);
+    },
+    deleteItem(state, action) {
+      //payload = pizzaId
+      state.cart = state.cart.filter((item) => item.pizzaId !== action.payload);
+    },
+    increaseItemQunantity(state, action) {
+        
+    },
+    decreaseItemQuantity(state, action) {},
+    clearCart(state, action) {},
+  },
+});
